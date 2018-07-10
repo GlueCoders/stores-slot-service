819aabe513e3f03cbe405fd628e8323ff0571228
Anand Rajneesh <anand.rajneesh@tesco.com>
2018-07-10 18:34:56 +0530

    [Setup] Testing travis to heroku pipeline

--- src/main/java/com/gluecoders/tesco/hackathon/storeslots/rest/resources/Ping.java
+++ src/main/java/com/gluecoders/tesco/hackathon/storeslots/rest/resources/Ping.java
@@ -2,6 +2,7 @@ package com.gluecoders.tesco.hackathon.storeslots.rest.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +15,8 @@ public class Ping {
         return ResponseEntity.ok().body("All is well");
     }
 
+    @PostMapping
+    public ResponseEntity ping1(){
+        return ResponseEntity.ok().body("All is well");
+    }
 }
