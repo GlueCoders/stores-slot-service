0a148060adece156f84721566a2ab1e1a93b7e6f
Anand Rajneesh <anandrajneesh@outlook.com>
2018-07-10 18:03:35 +0530

    [Setup] Travis CI to Heroku deployment link + Ping resource

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/store-slots-service-1.0-SNAPSHOT.jar
---
+++ src/main/java/com/gluecoders/tesco/hackathon/storeslots/rest/resources/Ping.java
@@ -0,0 +1,17 @@
+package com.gluecoders.tesco.hackathon.storeslots.rest.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/ping")
+public class Ping {
+
+    @GetMapping
+    public ResponseEntity ping(){
+        return ResponseEntity.ok().body("All is well");
+    }
+
+}
